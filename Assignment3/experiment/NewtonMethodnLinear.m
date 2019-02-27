5bfa79e3fca7f06058af147e4b2db91adfdf2448
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-27 18:50:07 -0500

    added the functions and renamed files to something meaningful

--- Assignment3/experiment/NewtonMethodnLinear.m
+++ Assignment3/experiment/NewtonMethodnLinear.m
@@ -1,15 +1,15 @@
 
-F = @ourfunction9 
+F = @functionEquation 
 J = @ourfunction10
 
-x0 = [0.1; 0.1; -0.1]
-A = J(x0)
-z0 = -F(x0)
-y0 = linsolve(A,z0)
-x1 = x0 + y0
-norm(y0,2)
-% This process is continued within a loop until the norm is lessw than 0.00001
-for j = 1:10
+x0 = [0.1; 0.1; -0.1];
+A = J(x0);
+z0 = -F(x0);
+y0 = linsolve(A,z0);
+x1 = x0 + y0;
+norm(y0,2);
+
+for j = 1:50
     x0 = x1;
     A = J(x0);
     z0 = -F(x0);
@@ -19,5 +19,5 @@ for j = 1:10
     fprintf(' %3d %15.10f %15.10f %15.10f %15.10f \n',j,x1(1),x1(2),x1(3),err);
     if err < 0.00001
         break;
-    end;
-end;
+    end
+end
