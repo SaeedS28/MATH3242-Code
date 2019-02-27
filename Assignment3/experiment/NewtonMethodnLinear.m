aa1ef191c835eeb993708f9d4794c9725a79e246
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-27 18:38:24 -0500

    A fresh start never hurt anyone

---
+++ Assignment3/experiment/NewtonMethodnLinear.m
@@ -0,0 +1,23 @@
+
+F = @ourfunction9 
+J = @ourfunction10
+
+x0 = [0.1; 0.1; -0.1]
+A = J(x0)
+z0 = -F(x0)
+y0 = linsolve(A,z0)
+x1 = x0 + y0
+norm(y0,2)
+% This process is continued within a loop until the norm is lessw than 0.00001
+for j = 1:10
+    x0 = x1;
+    A = J(x0);
+    z0 = -F(x0);
+    y0 = linsolve(A,z0);
+    x1 = x0 + y0;
+    err = norm(y0,2);
+    fprintf(' %3d %15.10f %15.10f %15.10f %15.10f \n',j,x1(1),x1(2),x1(3),err);
+    if err < 0.00001
+        break;
+    end;
+end;
