1cb34ad28442e9aa8ac4550b63b0b8fe598253e0
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-02-27 19:38:28 -0500

    this is the best I can do, The jacobian is a singular matrix

--- Assignment3/experiment/NewtonMethodnLinear.m
+++ Assignment3/experiment/NewtonMethodnLinear.m
@@ -1,23 +1,24 @@
+F = @functionEquation;
+J = @jacobian;
 
-F = @functionEquation 
-J = @ourfunction10
-
-x0 = [0.1; 0.1; -0.1];
+x0 = [3;3];
 A = J(x0);
 z0 = -F(x0);
 y0 = linsolve(A,z0);
 x1 = x0 + y0;
 norm(y0,2);
 
-for j = 1:50
+for j = 1:300
+    
     x0 = x1;
     A = J(x0);
     z0 = -F(x0);
     y0 = linsolve(A,z0);
     x1 = x0 + y0;
     err = norm(y0,2);
-    fprintf(' %3d %15.10f %15.10f %15.10f %15.10f \n',j,x1(1),x1(2),x1(3),err);
-    if err < 0.00001
+    
+    fprintf(' %3d %15.10f %15.10f %15.10f \n',j,x1(1),x1(2),err);
+    if err < 0.000001
         break;
     end
 end
